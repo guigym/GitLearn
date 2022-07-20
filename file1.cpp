12f58a0b07e5f4075b1ca5ab88dd292c5bb62698
chunbowu <chunbo.wu@intel.com>
2022-07-20 14:50:43 +0800

    upload some basic file;

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: file1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 20 Jul 2022 11:51:05 AM CST
+ ************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main() {
+   cout << 'here is the 1st cpp file\n';
+   return 0;
+}
